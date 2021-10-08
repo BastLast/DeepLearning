1f639b7c0bf379161d3bf5f5ca91c93156e9e853
GosuPaper <florian.girardin42@gmail.com>
2021-10-08 10:47:28 +0200

    test des trucs 2

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import imageio as imageio
 import numpy as np
 import sio as sio
 import torch
+from matplotlib import pyplot as plt
 from torch.autograd import Variable
 
 from torch.utils.data.dataloader import DataLoader
@@ -20,11 +21,13 @@ def adjust_learning_rate(optimizer, epoch):
 
     return lr
 
+
 def output_psnr_mse(img_orig, img_out):
     squared_error = np.square(img_orig - img_out)
     mse = np.mean(squared_error)
     psnr = 10 * np.log10(1.0 / mse)
     return psnr
+
 
 def train_optim(model, device, epochs, log_frequency, learning_rate=1e-4):
     model.to(device)  # we make sure the model is on the proper device
@@ -34,7 +37,7 @@ def train_optim(model, device, epochs, log_frequency, learning_rate=1e-4):
     optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
 
     for epoch in range(epochs):
-        lr = adjust_learning_rate(optimizer, epoch) # adjust the learning rate. Decreasing.
+        lr = adjust_learning_rate(optimizer, epoch)  # adjust the learning rate. Decreasing.
         for param_group in optimizer.param_groups:
             param_group["lr"] = lr
 
@@ -56,13 +59,15 @@ def train_optim(model, device, epochs, log_frequency, learning_rate=1e-4):
 
             if batch_id % log_frequency == 0:
                 print(
-                    "epoch: {:03d}, batch: {:03d}, loss: {:.3f}, time: {:.3f}".format(epoch + 1, batch_id + 1, loss.item(),
+                    "epoch: {:03d}, batch: {:03d}, loss: {:.3f}, time: {:.3f}".format(epoch + 1, batch_id + 1,
+                                                                                      loss.item(),
                                                                                       time.time() - start))
 
             optimizer.zero_grad()  # clear the gradient before backward
             loss.backward()  # update the gradient
 
             optimizer.step()  # update the model parameters using the gradient
+
 
         # Model evaluation after each step computing the accuracy
         model.eval()
@@ -80,24 +85,27 @@ def train_optim(model, device, epochs, log_frequency, learning_rate=1e-4):
                 out_pred = model(images)
                 elapsed_time = time.time() - start_time
                 avg_elapsed_time += elapsed_time
+                val = torch.abs(labels - out_pred).sum()
+                print(val)
+                if epoch == 3:
+                    plt.figure(1)
+                    plt.imshow(images[0].cpu().permute(1, 2, 0))
+                    plt.figure(2)
+                    plt.imshow(labels[0].cpu().permute(1, 2, 0))
+                    plt.figure(3)
+                    plt.imshow(out_pred[0].cpu().permute(1, 2, 0))
+                    plt.show()
+                '''
 
-                psnr_predicted = output_psnr_mse(labels, output)
-                avg_psnr_predicted += psnr_predicted
-                ct += 1
-                output = output * 255.
-                output = np.uint8(np.round(output))
-
-                test_name = "originname" + str(batch_id) + '.png'  # .mat -> .png
-                imageio.imwrite("./result/result_images/" + test_name, output)  # save result images
-
-                print(100 * ct / (testloader.__len__()), "percent done")
 
         avg_psnr_predicted = avg_psnr_predicted / ct
         avg_psnr_noisy = avg_psnr_noisy / ct
 
         print("PSNR_noisy=", avg_psnr_noisy)
         print("PSNR_predicted=", avg_psnr_predicted)
+        '''
 
+    torch.save(model.state_dict(), './modeltrained')
 
 if __name__ == '__main__':
     device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
@@ -106,11 +114,11 @@ if __name__ == '__main__':
     t_dataset = "./dataset/train_1A_tiny.npy"
     dataset = LoadImages(t_dataset, o_dataset)
     batch_size = 16
-    #dataloader = DataLoader(dataset, batch_size, shuffle=True)
+    # dataloader = DataLoader(dataset, batch_size, shuffle=True)
     trainloader = DataLoader(dataset, batch_size=batch_size, shuffle=True, num_workers=2)
     testloader = DataLoader(dataset, batch_size=batch_size, shuffle=False, num_workers=2)
     model = CustomModel()
-    nb_epoch = 1
+    nb_epoch = 3
     log_frequency = 10
     learning_rate = 1e-4
     train_optim(model, device, nb_epoch, log_frequency, learning_rate)
