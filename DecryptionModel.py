78ed077c3e2f56e6126273d44c50abc04eedb588
BastLast <bastien.last87@gmail.com>
2021-10-09 11:56:17 +0200

    t2 3000

--- DecryptionModel.py
+++ DecryptionModel.py
@@ -9,22 +9,28 @@ class DecryptionModel(nn.Module):
         self.flatten = nn.Flatten(start_dim=1)
         self.conv1 = nn.Conv2d(in_channels=3, out_channels=1, kernel_size=1, stride=1, padding=0, bias=False)
         self.conv2 = nn.Conv2d(in_channels=1, out_channels=3, kernel_size=1, stride=1, padding=0, bias=False)
-        self.relu1 = nn.ReLU()
+        self.relu1 = nn.PReLU()
         self.linear = nn.Linear(9216, 9216)
         # self.unFlatten = nn.UnFlatten()
 
     def forward(self, x):
-        print("x shape : {}".format(x.shape))
+        # print("x shape : {}".format(x.shape))
         x = x.permute(1, 0, 2, 3)
-        print("x shape : {}".format(x.shape))
+        # print("x shape : {}".format(x.shape))
         blue = x[0]
         green = x[1]
         red = x[2]
-        print("blue shape : {}".format(blue.shape))
+        mix = torch.add(blue,green)
+        mix = torch.add(mix,red)
+        # print("blue shape : {}".format(blue.shape))
+        mix = self.flatten(mix)
+        mix = self.linear(mix)
+        mix = self.relu1(mix)
+        # print("blue shape : {}".format(blue.shape))
         blue = self.flatten(blue)
         blue = self.linear(blue)
         blue = self.relu1(blue)
-
+        # self.linear().requires_grad = False
         green = self.flatten(green)
         green = self.linear(green)
         green = self.relu1(green)
@@ -36,10 +42,10 @@ class DecryptionModel(nn.Module):
         blue = torch.reshape(blue, [blue.shape[0], 96, 96])
         green = torch.reshape(green, [green.shape[0], 96, 96])
         red = torch.reshape(red, [red.shape[0], 96, 96])
-        print("blue shape : {}".format(blue.shape))
-        print("x shape : {}".format(x.shape))
+        #  print("blue shape : {}".format(blue.shape))
+        # print("x shape : {}".format(x.shape))
         x = torch.stack((blue, green, red))
-        print("x shape : {}".format(x.shape))
+        # print("x shape : {}".format(x.shape))
         x = x.permute(1, 0, 2, 3)
-        print("x shape : {}".format(x.shape))
+        # print("x shape : {}".format(x.shape))
         return x
